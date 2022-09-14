b9f22fe024b57832ab8691030da80409288d96a3
jo-joanna <b.joanna.uk@gmail.com>
2022-09-14 22:43:47 +0200

    Update a content

--- src/screens/home.js
+++ src/screens/home.js
@@ -7,15 +7,15 @@ export default function Home() {
       <ScrollView>
         <StatusBar style="auto" />
             <View style={styles.container}>
-        <Text style={styles.title}>Welcome in my Mobile Photo Blog</Text>
-        <Text style={styles.text}>My name is Joanna. I would like to invite you to travel with me across Spain. At the moment we're visiting Granada and Malaga.</Text>
-        <Text style={styles.text}>Menu is intuitive. At the bottom, you will find icons to move to 3 main sections.</Text>
-        <Text>- Home (we're here now) {'\n'}
-            - Gallery (you will have a menu with the destinations) {'\n'}
-            - Contact (you can ask me anything about an app and journeys! )
-        </Text>
-        <Image source={{uri: 'https://royaltrendia.com/wp-content/uploads/2018/07/travel-globe-uk-flights.png'}}
-            style={{width: 300, height: 250, alignItems: 'center'}}/>
+                <Text style={styles.title}>Welcome in my Mobile Photo Blog</Text>
+                <Text style={styles.text}>My name is Joanna. I would like to invite you to travel with me across Spain. At the moment we're visiting Granada and Malaga.</Text>
+                <Text style={styles.text}>Menu is intuitive. At the bottom, you will find icons to move to 3 main sections.</Text>
+                <Text>- Home (we're here now) {'\n'}
+                    - Gallery (you will have a menu with the destinations) {'\n'}
+                    - Contact (you can ask me anything about an app and journeys! )
+                </Text>
+                <Image source={{uri: 'https://royaltrendia.com/wp-content/uploads/2018/07/travel-globe-uk-flights.png'}}
+                    style={{width: 300, height: 250, alignItems: 'center'}}/>
             </View>
   
       </ScrollView>
