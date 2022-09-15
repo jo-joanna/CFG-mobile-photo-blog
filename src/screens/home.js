de76bc9771d16087dcc0452fdf2b0970416ad1b9
jo-joanna <b.joanna.uk@gmail.com>
2022-09-15 23:38:21 +0200

    Change wording, add padding

--- src/screens/home.js
+++ src/screens/home.js
@@ -12,7 +12,7 @@ export default function Home() {
                 <Text style={styles.text}>My name is Joanna. I would like to invite you to travel with me across Spain. At the moment we're visiting Granada and Malaga.</Text>
                 <Text style={styles.text}>Menu is intuitive. At the bottom, you will find icons to move to 3 main sections.</Text>
                 <Text style={styles.text}>- Home (we're here now) {'\n'}
-                    - Gallery (you will have a menu with the destinations) {'\n'}
+                    - Gallery (at the moment Granada. More destination will be added soon) {'\n'}
                     - Contact (you can ask me anything about an app and journeys! )
                 </Text>
                 <Image source={{uri: 'https://royaltrendia.com/wp-content/uploads/2018/07/travel-globe-uk-flights.png'}}
@@ -31,6 +31,7 @@ export default function Home() {
       backgroundColor: '#FFB703',
       color: '#023047',
       alignItems: 'center',
+      padding: 20,
 
     },
     title: {
