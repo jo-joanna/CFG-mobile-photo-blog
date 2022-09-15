1df1f5fdc018a46fca19e1749eb345d00654c140
jo-joanna <b.joanna.uk@gmail.com>
2022-09-15 23:39:56 +0200

    Create a component Card/UI

--- src/components/commentCard.js
+++ src/components/commentCard.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
+import React from "react";
+import { View, StyleSheet } from "react-native";
+// import {Card} from 'react-native-paper';
 
 export default function Card(props) {
-    return (
-        <View style={styles.card}>
-            <View style={styles.cardContent}>
-                {props.children}
-                </View></View>
-    )
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>
+        {props.children}
+      </View>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-    card: {
-        elevation: 3,
-        backgroundColor: '#FB8500',
-        shadowOffset: {width: 3, height: 1},
-        shadowColor: '#FB8500',
-        shadowOpacity: 0.2,
-        margin: 25,
-    },
-    cardContent: {
-        marginHorizontal: 18,
-        marginVertical: 10,
-    }
-
-})
+const styles = StyleSheet.create({
+  card: {
+    elevation: 8,
+    backgroundColor: "#FB8500",
+    shadowOffset: { width: 10, height: 6 },
+    shadowColor: "#023047",
+    shadowOpacity: 0.2,
+    margin: 25,
+  },
+  cardContent: {
+    marginHorizontal: 18,
+    marginVertical: 10,
+  },
+});
--- src/screens/comments.js
+++ src/screens/comments.js
@@ -1,53 +1,60 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+// import {Card} from 'react-native-paper';
+
 import Card from '../components/commentCard';
 
-export default function Comments({navigation}){
-
-    const [reviews, setReviews] = useState([
-        {name: 'Anna', com: '"This website is great!"', date: '25/02/2022'},
-        {name: 'Ken', com: '"I recommend to go to Malaga"', date: '10/05/2022'},
-        {name: 'Adam', com: '"It\'s a fantastic place"', date: '14/06/2022'},
-        {name: 'Lee', com: '"This website is great!"', date: '15/05/2021'},
-        {name: 'Eleonor', com: '"I am expecting more photos"', date: '15/09/2021'},
-    ])
-    return(
-        <View style={styles.container}>
-            <FlatList
-                data={reviews}
-                renderItem={({item}) => (
+export default class Comments extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            List: [
+                {name: 'Anna', com: '"This website is great!"', date: '25/02/2022'},
+                {name: 'Ken', com: '"I recommend to go to Malaga"', date: '10/05/2022'},
+                {name: 'Adam', com: '"It\'s a fantastic place"', date: '14/06/2022'},
+                {name: 'Lee', com: '"This website is great!"', date: '15/05/2021'},
+                {name: 'Eleonor', com: '"I am expecting more photos"', date: '15/09/2021'}
+            ]
+        };
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+                <FlatList
+                data={this.state.List}
+                renderItem={({item}) =>
                     <Card>
-                        <Text style={styles.name}>{item.name}{item.date}</Text>
+                        <Text style={styles.date}>{item.date}</Text>
+                        <Text style={styles.name}>{item.name}</Text>
                         <Text style={styles.com}>{item.com}</Text>
                     </Card>
-                   
-                )}
+                }
+                keyExtractor={item=>item.name}
+                  
                 />
-           
-        </View>
-    )
+            </View>
+        )
+    }
 }
+
+
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        backgroundColor: '#FFB703'},
+        backgroundColor: '#FFB703',
+    },
     name: {
-        fontWeight: 500,
+        fontWeight: 'bold',
+        fontSize: 15,
+        lineHeight: 30,
     },
     com: {
         fontStyle: 'italic',
     },
     date:{
-        fontWeight: 700,
+        fontWeight: 'bold',
         alignSelf: 'flex-end',
     },
   });
-
-//   <TouchableOpacity onPress={()=>navigation.navigate('ReviewDetails', item)}>
-//   <Card>
-//       <Text style={styles.name}>{item.name}{item.date}</Text>
-//       <Text style={styles.com}>{item.com}</Text>
-//   </Card>
-
-// </TouchableOpacity>
