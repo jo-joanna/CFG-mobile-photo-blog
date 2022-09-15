498eb71bcd667ccb71a38eb893cedb629c4a673a
jo-joanna <b.joanna.uk@gmail.com>
2022-09-15 05:12:30 +0200

    Add icons

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './src/screens/home';
-import GalleryMenu from './src/screens/galleryMenu';
+import Granada from './src/screens/galleryGranada';
 import Contact from './src/screens/contact';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import Comments from './src/screens/comments';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,10 +13,44 @@ export default function App() {
    
       <NavigationContainer>
   
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Gallery" component={GalleryMenu} />
-          <Tab.Screen name="Contact" component={Contact} />
+        <Tab.Navigator
+                activeColor="#FB8500"
+                labelStyle={{ fontSize: 15 }}
+                style={{ backgroundColor: 'green' }}
+                //this has to be added otherwise it does not change a color
+                screenOptions={({ }) => ({
+                  tabBarInactiveTintColor: "#f48b28",
+                  tabBarActiveTintColor: "#023047"
+                })}
+                >
+          <Tab.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                      tabBarLabel: 'Home',
+                      tabBarIcon: ({ color }) => (
+                        <MaterialIcon name="home" color={color} size={30} />
+                      ),
+                    }}
+                  />
+          <Tab.Screen name="Gallery" component={Granada} options={{
+                      tabBarLabel: 'Gallery',
+                      tabBarIcon: ({ color }) => (
+                        <MaterialIcon name="photo-library" color={color} size={30} />
+                      ),
+                    }}/>
+          <Tab.Screen name="Comments" component={Comments} options={{
+                      tabBarLabel: 'Comments',
+                      tabBarIcon: ({ color }) => (
+                        <MaterialIcon name="speaker-notes" color={color} size={30} />
+                      ),
+                    }}/>
+          <Tab.Screen name="Contact" component={Contact} options={{
+                      tabBarLabel: 'Contact',
+                      tabBarIcon: ({ color }) => (
+                        <MaterialIcon name="email" color={color} size={30} />
+                      ),
+                    }}/>
         </Tab.Navigator>
            
       </NavigationContainer>
