f1594a39a0cb1c55a55dbc39a3cabbe7bb4339fb
jo-joanna <b.joanna.uk@gmail.com>
2022-09-20 02:02:09 +0200

    Structure change
    App structure change to be able to display nested navigations. Navigations are in folders separated.

--- App.js
+++ App.js
@@ -1,59 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './src/screens/home';
-import Granada from './src/screens/galleryGranada';
-import Contact from './src/screens/contact';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-import Comments from './src/screens/comments';
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-const Tab = createBottomTabNavigator();
+import StackNavigation from './src/StackNavigation';
+
 
 export default function App() {
-  return (
-   
-      <NavigationContainer>
-  
-        <Tab.Navigator
-                activeColor="#FB8500"
-                labelStyle={{ fontSize: 15 }}
-                style={{ backgroundColor: 'green' }}
-                //this has to be added otherwise it does not change a color
-                screenOptions={({ }) => ({
-                  tabBarInactiveTintColor: "#f48b28",
-                  tabBarActiveTintColor: "#023047"
-                })}
-                >
-          <Tab.Screen
-                    name="Home"
-                    component={Home}
-                    options={{
-                      tabBarLabel: 'Home',
-                      tabBarIcon: ({ color }) => (
-                        <MaterialIcon name="home" color={color} size={30} />
-                      ),
-                    }}
-                  />
-          <Tab.Screen name="Gallery" component={Granada} options={{
-                      tabBarLabel: 'Gallery',
-                      tabBarIcon: ({ color }) => (
-                        <MaterialIcon name="photo-library" color={color} size={30} />
-                      ),
-                    }}/>
-          <Tab.Screen name="Comments" component={Comments} options={{
-                      tabBarLabel: 'Comments',
-                      tabBarIcon: ({ color }) => (
-                        <MaterialIcon name="speaker-notes" color={color} size={30} />
-                      ),
-                    }}/>
-          <Tab.Screen name="Contact" component={Contact} options={{
-                      tabBarLabel: 'Contact',
-                      tabBarIcon: ({ color }) => (
-                        <MaterialIcon name="email" color={color} size={30} />
-                      ),
-                    }}/>
-        </Tab.Navigator>
-           
-      </NavigationContainer>
-         
+    return (
+        <NavigationContainer style={{flex: 1}}>
+            <StatusBar style="auto" />
+             <StackNavigation />
+
+        </NavigationContainer>
     );
-  }
+};
+
