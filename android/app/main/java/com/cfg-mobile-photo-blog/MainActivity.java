b761dbb1dd59426415e5a9c76e56f4110b99a993
jo-joanna <b.joanna.uk@gmail.com>
2022-09-20 02:01:01 +0200

    for Nested navigation implementation

---
+++ android/app/main/java/com/cfg-mobile-photo-blog/MainActivity.java
@@ -0,0 +1,15 @@
+package com.authentication;
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return 'Cfg-mobile-photo-blog'
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(null);
+    }
+}
