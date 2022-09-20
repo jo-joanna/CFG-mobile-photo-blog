f8df0a76f4211829bb42b7d8d9e012ef992db816
jo-joanna <b.joanna.uk@gmail.com>
2022-09-20 02:11:30 +0200

    Create users to validate against them

---
+++ src/screens/users.js
@@ -0,0 +1,18 @@
+let Users=[{
+    username: 'Anna',
+    email: 'anna@gmail.com',
+    password: 'anna1',
+},
+{
+    username: 'Mani',
+    email: 'mani1@gmail.com',
+    password: 'mani1',
+        
+},
+{
+    username: 'Adam',
+    email: 'adi@gmail.com',
+    password: 'adem12',
+        
+},
+];
